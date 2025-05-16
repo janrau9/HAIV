56f996e38e47542240cf02241b897da631b35c46
ollikarejoki <olli.karejoki@gmail.com>
2025-05-16 22:14:55 +0300

    changed bg and added table

--- frontend/src/components/Background.tsx
+++ frontend/src/components/Background.tsx
@@ -10,9 +10,9 @@ import {motion} from 'framer-motion'
 export const Background:React.FC = () => {
 	return (
 		<motion.div
-		className='bg-black w-screen h-screen object-contain'
+		className='bg-black w-screen h-screen object-cover'
 		>
-			<img src="/images/wall.png" className="w-full h-full"></img>
+			<img src="/images/walls.png" className="min-w-full min-h-full"></img>
 		</motion.div>
 	);
   };
