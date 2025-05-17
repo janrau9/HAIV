69a7bc580b72a4abebf9134b8f12114eac4a3bba
Your Name <you@example.com>
2025-05-17 15:59:00 +0400

    Added nix flake and lock files for npm

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "HAIV dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }@inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_22
+          ];
+        };
+      }
+    );
+}
