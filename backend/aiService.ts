79299e8fd56844fc716090eb58c24ba1d4dc5437
Janrau Beray <janraup356@gmail.com>
2025-05-17 08:07:16 +0000

    added suspects object

--- backend/aiService.ts
+++ backend/aiService.ts
@@ -31,7 +31,7 @@ export async function askSuspect(
   You are being interrogated by a detective.
   you will give answers based on your characteristics.`;
 
-  const resp = openai_obj.responses.create({
+  const resp = await openai_obj.responses.create({
     model: "gpt-4.1-nano",
     input: [
       {
@@ -44,7 +44,6 @@ export async function askSuspect(
       },
     ],
   });
-
   return (resp);
 }
 
